20a010746785aa3c0257bb4b0f25b7d42bde02ce
superstones <292454993@qqcom>
2018-05-11 15:38:41 +0800

    Initial commit

---
+++ Test/Test.py
@@ -0,0 +1,17 @@
+d = {'Stone': 90, 'Shock': 95, 'super': 98}
+print(d['Stone'])
+print('Stone' in d)
+print('S' in d)
+print(d.get('Stone'))
+print(d.get('S'))
+print(d.get('S', -1))
+s = set([1, 2, 3])
+s.add(4)
+print(s)
+s.remove(4)
+print(s)
+x = abs(-100)
+print(x)
+n1 = 225
+n2 = 1000
+print(hex(n1),hex(n2))
